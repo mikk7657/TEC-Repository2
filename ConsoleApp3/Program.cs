726b6d6527f3953c34fc47dd59205a56f57ec7b2
mikk7657 <39336080+mikk7657@users.noreply.github.com>
2018-05-16 14:19:00 +0200

    Add files via upload
    Import file 2

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\nIndtast dit navn :");
+            List<string> navne = new List<string>();
+            //navne.
+
+            string navn = Console.ReadLine(); // her er alle navne afskildt med "space"
+            string[] delnavne = navn.Split(' ');
+            string samletNavn = "";
+            foreach (string delafnavn in delnavne)
+
+            {
+               // Console.WriteLine("Del :" + StortOgSmåt(delafnavn));
+                samletNavn += stortOgSmåt(delafnavn) + " ";
+            }
+            Console.WriteLine(samletNavn);
+            Console.ReadLine();
+
+        }
+        //metode der modatager del af navn og retunerer delafnavn med stort 1. bogstav og resten småt.
+        static string stortOgSmåt(string delnavn)
+        {
+            return delnavn.Substring(0, 1).ToUpper() + delnavn.Substring(1).ToLower();
+        }
+    }
+}
